d68cf83592074c48b9cf2dd9856ea7364a824218
CatchZeng <891793848@qq.com>
2018-12-20 11:56:45 +0800

    feat: multipart upload

--- web-upload/main.go
+++ web-upload/main.go
@@ -33,5 +33,27 @@ func main() {
 		c.String(http.StatusOK, fmt.Sprintf("文件 %s 上传成功。参数 name=%s，email=%s.", file.Filename, name, email))
 	})
 
+	router.POST("/multipleUpload", func(c *gin.Context) {
+		name := c.PostForm("name")
+		email := c.PostForm("email")
+
+		form, err := c.MultipartForm()
+		if err != nil {
+			c.String(http.StatusBadRequest, fmt.Sprintf("上传失败: %s", err.Error()))
+			return
+		}
+		files := form.File["files"]
+
+		for _, file := range files {
+			filename := filepath.Base(file.Filename)
+			if err := c.SaveUploadedFile(file, filename); err != nil {
+				c.String(http.StatusBadRequest, fmt.Sprintf("上传错误: %s", err.Error()))
+				return
+			}
+		}
+
+		c.String(http.StatusOK, fmt.Sprintf("上传成功 %d 文件。 参数 name=%s，email=%s.", len(files), name, email))
+	})
+
 	router.Run(":8080")
 }
